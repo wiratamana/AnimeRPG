800d049bbbffd19851a94827cba840c2717ce63f
Wiratamana <wiratamana@icloud.com>
2019-04-22 02:15:52 +0900

    SingletonMonobehaviour

---
+++ AnimeRPG/Assets/Scripts/SingletonMonobehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tamana
+{
+    public class SingletonMonobehaviour<T> : MonoBehaviour
+    {
+        public static T instance { protected set; get; }
+
+        protected virtual void Awake()
+        {
+            instance = gameObject.GetComponent<T>();
+        }
+    }
+
+}
